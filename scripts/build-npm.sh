8152941ba95cb0b7700c88ff97cd503b63c04651
Jarvan <j532547613@gmail.com>
2023-08-25 10:50:55 +0800

    feat: update

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -1,9 +1,9 @@
 rm -rf lib
 
 echo "1"
-babel -d lib ./modules
+npx babel -d lib ./modules
 echo "2"
-sass css/:lib/css/ --no-source-map --style=expanded
+npx sass css/:lib/css/ --no-source-map --style=expanded
 echo "3"
 cp css/antd.less lib/css/antd.less
 
