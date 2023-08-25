18bcb5766ebd51aa0a733e14e000e5a82e31068f
Jarvan <j532547613@gmail.com>
2023-08-25 14:07:12 +0800

    feat: update

--- modules/config/basic.js
+++ modules/config/basic.js
@@ -785,8 +785,8 @@ const widgets = {
     },
     spelFormatValue: (val, fieldDef, wgtDef, op, opDef) => {
       const dateVal = moment(val, wgtDef.valueFormat);
-      // return `new java.text.SimpleDateFormat('yyyy-MM-dd').parse('${dateVal.format("YYYY-MM-DD")}')`;
-      return `string_to_date('${dateVal.format("YYYY-MM-DD")}', 'yyyy-MM-dd')`;
+      return `new java.text.SimpleDateFormat('yyyy-MM-dd').parse('${dateVal.format("YYYY-MM-DD")}')`;
+      // return `string_to_date('${dateVal.format("YYYY-MM-DD")}', 'yyyy-MM-dd')`;
     },
     jsonLogic: (val, fieldDef, wgtDef) => moment(val, wgtDef.valueFormat).toDate(),
     toJS: (val, fieldSettings) => {
@@ -823,8 +823,8 @@ const widgets = {
     },
     spelFormatValue: (val, fieldDef, wgtDef, op, opDef) => {
       const dateVal = moment(val, wgtDef.valueFormat);
-      // return `T(java.time.LocalTime).parse('${dateVal.format("HH:mm:ss")}')`;
-      return `string_to_date('${dateVal.format("HH:mm:ss")}', 'HH:mm:ss')`;
+      return `T(java.time.LocalTime).parse('${dateVal.format("HH:mm:ss")}')`;
+      // return `string_to_date('${dateVal.format("HH:mm:ss")}', 'HH:mm:ss')`;
     },
     jsonLogic: (val, fieldDef, wgtDef) => {
       // return seconds of day
@@ -881,8 +881,8 @@ const widgets = {
     },
     spelFormatValue: (val, fieldDef, wgtDef, op, opDef) => {
       const dateVal = moment(val, wgtDef.valueFormat);
-      // return `new java.text.SimpleDateFormat('yyyy-MM-dd HH:mm:ss').parse('${dateVal.format("YYYY-MM-DD HH:mm:ss")}')`;
-      return `string_to_date('${dateVal.format("YYYY-MM-DD HH:mm:ss")}', 'yyyy-MM-dd HH:mm:ss')`;
+      return `new java.text.SimpleDateFormat('yyyy-MM-dd HH:mm:ss').parse('${dateVal.format("YYYY-MM-DD HH:mm:ss")}')`;
+      // return `string_to_date('${dateVal.format("YYYY-MM-DD HH:mm:ss")}', 'yyyy-MM-dd HH:mm:ss')`;
     },
     jsonLogic: (val, fieldDef, wgtDef) => moment(val, wgtDef.valueFormat).toDate(),
     toJS: (val, fieldSettings) => {
